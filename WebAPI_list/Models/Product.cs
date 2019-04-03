4abaf77e29120656fed39576d53ec3508992c47f
Taner <47195442+tanerurun@users.noreply.github.com>
2019-04-04 01:49:03 +0300

    Add files via upload

---
+++ WebAPI_list/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI_list.Models
+{
+    public class Product
+    {
+        //1.ADIM
+        //burada nesnemizi oluşturduk ilk önce model dosyası içersinde properties 
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
